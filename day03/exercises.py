9737344a04e7df3a9069a4a8a26c278b9f6750e8
Rana Saymah Manharsinh <saymahrana30@gmail.com>
2026-09-08 15:45:46 +0530

    Add grade calculator functionality

--- day03/exercises.py
+++ day03/exercises.py
@@ -6,3 +6,24 @@ Attempt this yourself before checking solutions.py.
 """
 
 # Your code here
+print("===== GRADE CALCULATOR ======")
+
+marks = float(input("Enter your marks: "))
+
+if not 0 <= marks <= 100:
+    print("Invalid marks! Enter a value between 0 and 100.")
+
+elif marks >= 90:
+    print("Your grade is: A")
+
+elif marks >= 80:
+    print("Your grade is: B")
+
+elif marks >= 70:
+    print("Your grade is: C")
+
+elif marks >= 60:
+    print("Your grade is: D")
+
+else:
+    print("Your grade is: F")
